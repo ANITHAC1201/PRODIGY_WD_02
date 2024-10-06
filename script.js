e252c1c0ac96531781c4f5a138848e3178cdb255
ANITHAC1201 <anithac1201@gmail.com>
2024-10-06 21:51:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+let startTime, updatedTime, difference, tInterval;
+let running = false;
+let lapCount = 0;
+
+const display = document.getElementById('display');
+const startBtn = document.getElementById('startBtn');
+const pauseBtn = document.getElementById('pauseBtn');
+const resetBtn = document.getElementById('resetBtn');
+const lapBtn = document.getElementById('lapBtn');
+const lapList = document.getElementById('lapList');
+
+startBtn.addEventListener('click', startTimer);
+pauseBtn.addEventListener('click', pauseTimer);
+resetBtn.addEventListener('click', resetTimer);
+lapBtn.addEventListener('click', recordLap);
+
+function startTimer() {
+    if (!running) {
+        startTime = new Date().getTime() - (difference || 0);
+        tInterval = setInterval(updateTime, 10);
+        running = true;
+    }
+}
+
+function updateTime() {
+    updatedTime = new Date().getTime();
+    difference = updatedTime - startTime;
+
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    display.innerText = (hours < 10 ? "0" : "") + hours + ":" +
+                        (minutes < 10 ? "0" : "") + minutes + ":" +
+                        (seconds < 10 ? "0" : "") + seconds;
+}
+
+function pauseTimer() {
+    clearInterval(tInterval);
+    running = false;
+}
+
+function resetTimer() {
+    clearInterval(tInterval);
+    running = false;
+    difference = 0;
+    display.innerText = "00:00:00";
+    lapCount = 0;
+    lapList.innerHTML = '';  // Clear laps
+}
+
+function recordLap() {
+    if (running) {
+        lapCount++;
+        const lapTime = display.innerText;
+        const lapItem = document.createElement('div');
+        lapItem.innerText = `Lap ${lapCount}: ${lapTime}`;
+        lapList.appendChild(lapItem);
+    }
+}
